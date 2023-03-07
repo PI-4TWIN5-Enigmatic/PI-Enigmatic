be98cd0eadca3c8c235a6df4a097fe7bb4eb3958
siwar enfaiedh <siwar.enfaiedh@esprit.tn>
2023-03-07 15:07:40 +0100

    login first commit
    User Management Login

---
+++ projet backend/route/user.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const {signup,login} = require("../controllers/user")
+
+
+
+
+
+
+router.post('/signup', signup)
+
+router.post('/login', login,Authorization)
+
+module.exports = router;
