ba8e87bf70890c9c7592e20f06a03da0297b1323
siwar enfaiedh <92674741+siwar234@users.noreply.github.com>
2023-03-08 21:17:45 +0100

    Update user.js

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -30,6 +30,26 @@ exports.signup = async (req,res,next) =>{
    }
 }
 
+/* LOGGING IN */
+exports.login = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email: email });
+      if (!user) return res.status(400).json({ msg: "User does not exist. " });
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+
+  
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      delete user.password;
+      res.status(200).json({ token, user });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  };
+
+
 //get list user 
 exports.getListUser = async (req, res,next) => {
     try {
@@ -39,3 +59,5 @@ exports.getListUser = async (req, res,next) => {
        res.status(404).json({message: error.message});
     }
 }
+
+
