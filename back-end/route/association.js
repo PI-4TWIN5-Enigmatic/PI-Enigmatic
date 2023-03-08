b6a46db042f8ee55307dfb8c69d7c20254024807
siwar enfaiedh <92674741+siwar234@users.noreply.github.com>
2023-03-08 21:20:28 +0100

    Update association.js

--- back-end/route/association.js
+++ back-end/route/association.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {signupAssociation , getListAssociation} = require("../controllers/association")
+const {signupAssociation , getListAssociation,loginassociation} = require("../controllers/association")
 
 
 
 router.post('/signupAssociation', signupAssociation)
+router.post('/loginassociation', loginassociation)
+
 
   //get list association
   router.get('/getAll',getListAssociation)
