cc9916c526e058f5d9fa83f1a1a2395ce586636a
siwar enfaiedh <92674741+siwar234@users.noreply.github.com>
2023-05-11 09:04:36 +0100

    Update Meeting.js

--- front-end/src/components/Acceuil/Meeting.js
+++ front-end/src/components/Acceuil/Meeting.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
+
+import { Link } from 'react-router-dom';
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
-import { useCookies } from "react-cookie";
-// import { Navigate, useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+
 function randomID(len) {
   let result = '';
   if (result) return result;
-  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+  var chars =
+    '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
     maxPos = chars.length,
     i;
   len = len || 5;
@@ -15,19 +18,19 @@ function randomID(len) {
   return result;
 }
 
-export function getUrlParams(
-  url = window.location.href
-) {
+export function getUrlParams(url = window.location.href) {
   let urlStr = url.split('?')[1];
   return new URLSearchParams(urlStr);
 }
 
 export default function Meeting() {
-//     const navigate=useNavigate();
-  const [_, setCookies] = useCookies(["roomID"]);
+  const [cookies, setCookies] = useCookies(['roomID']);
 
-  const roomID = getUrlParams().get('roomID') || randomID(5);
-  setCookies("roomID", roomID);
+  // Retrieve the room ID from local storage or generate a new one
+  const roomID = localStorage.getItem('roomID') || randomID(5);
+  localStorage.setItem('roomID', roomID);
+  setCookies('roomID', roomID);
+
   let role_str = getUrlParams(window.location.href).get('role') || 'Host';
   const role =
     role_str === 'Host'
@@ -41,57 +44,57 @@ export default function Meeting() {
     sharedLinks.push({
       name: 'Join as co-host',
       url:
-        window.location.protocol + '//' + 
-        window.location.host + window.location.pathname +
+        window.location.protocol +
+        '//' +
+        window.location.host +
+        window.location.pathname +
         '?roomID=' +
         roomID +
         '&role=Cohost',
     });
   }
+
+  // Generate the audience link using the room ID
+  const audienceLink = `${window.location.protocol}//${window.location.host}/stream?roomID=${roomID}&role=Audience`;
   sharedLinks.push({
     name: 'Join as audience',
-    url:
-     window.location.protocol + '//' + 
-     window.location.host + window.location.pathname +
-      '?roomID=' +
-      roomID +
-      '&role=Audience',
+    url: audienceLink,
   });
- // generate Kit Token
+
+  // Generate Kit Token
   const appID = 1456019005;
-  const serverSecret = "45a02972fc23a0c7b150ea30b36253cf";
-  const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
+  const serverSecret = '45a02972fc23a0c7b150ea30b36253cf';
+  const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+    appID,
+    serverSecret,
+    roomID,
+    randomID(5),
+    randomID(5)
+  );
 
   // start the call
   let myMeeting = async (element) => {
-      // Create instance object from Kit Token.
-      const zp = ZegoUIKitPrebuilt.create(kitToken);
-      // start the call
-      zp.joinRoom({
-        container: element,
-        scenario: {
-          mode: ZegoUIKitPrebuilt.LiveStreaming,
-          config: {
-            role,
-          },
+    // Create instance object from Kit Token.
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    // start the call
+    zp.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.LiveStreaming,
+        config: {
+          role,
         },
-        sharedLinks,
-      });
-        
-
+      },
+      sharedLinks,
+    });
   };
-
-
 
   return (
     <>
-     <div
-      className="myCallContainer"
-      ref={myMeeting}
-      style={{ width: '100vw', height: '100vh' }}
-    >
-    </div>
-    </>
-   
-  );
-}
+       <div
+    className="myCallContainer"
+    ref={myMeeting}
+    style={{ width: '100vw', height: '100vh' }}
+  ></div>
+
+      </>)}
